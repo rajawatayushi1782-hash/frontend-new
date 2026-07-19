758762cb27d8a5e81ea6291d0761fede785adf58
Ayushi Rajawat <rajawatayushi1782@gmail.com>
2026-07-19 09:31:42 +0530

    Responsive navbar with mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import "../styles/navbar.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
 
@@ -9,26 +12,50 @@ function Navbar() {
         Interview<span>Ace</span>
       </Link>
 
-      <ul className="nav-links">
-        <li><a href="#hero">Home</a></li>
-        <li><a href="#features">Features</a></li>
-        <li><a href="#faq">FAQ</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
+      <div
+        className="hamburger"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </div>
 
-      <div className="nav-btns">
+      <div className={`nav-menu ${menuOpen ? "active" : ""}`}>
 
-        <Link to="/login">
-          <button className="login-btn">
-            Login
-          </button>
-        </Link>
+        <ul className="nav-links">
+          <li>
+            <a href="#hero" onClick={() => setMenuOpen(false)}>
+              Home
+            </a>
+          </li>
 
-        <Link to="/signup">
-          <button className="signup-btn">
-            Sign Up
-          </button>
-        </Link>
+          <li>
+            <a href="#features" onClick={() => setMenuOpen(false)}>
+              Features
+            </a>
+          </li>
+
+          <li>
+            <a href="#faq" onClick={() => setMenuOpen(false)}>
+              FAQ
+            </a>
+          </li>
+
+          <li>
+            <a href="#contact" onClick={() => setMenuOpen(false)}>
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        <div className="nav-btns">
+          <Link to="/login">
+            <button className="login-btn">Login</button>
+          </Link>
+
+          <Link to="/signup">
+            <button className="signup-btn">Sign Up</button>
+          </Link>
+        </div>
 
       </div>
 
